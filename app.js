03e6bcf03dce68f04931b0a5d628ec6c5367e4f2
sumitmishrathinksys <mishra.sumit@thinksys.com>
2025-04-23 22:32:01 +0530

    install react and react-dom and understand parcel and vite bundler

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-const element = React.createElement("h1", {}, "Hello Coder Army");
-
+const element = React.createElement(
+  "h1",
+  {
+    id: "first",
+    className: "Rahul",
+    style: { backgroundColor: "blue", fontSize: "30px", color: "pink" },
+  },
+  "Hello Coder Army"
+);
+const element2 = React.createElement(
+  "h2",
+  {
+    id: "second",
+    className: "Rahul",
+    style: { backgroundColor: "black", fontSize: "25px", color: "white" },
+  },
+  "Kaise ho aap sab log"
+);
+const div1 = React.createElement(
+  "div",
+  {
+    id: "first",
+    className: "Rahul",
+    style: { backgroundColor: "blue", fontSize: "30px", color: "pink" },
+  },
+  [element, element2]
+);
 // ReactDOM.render(element, document.getElementById("root"));
 
 const Reactroot = ReactDOM.createRoot(document.getElementById("root"));
-Reactroot.render(element);
-
+// Reactroot.render(element);
+// Reactroot.render(element2);
+Reactroot.render(div1);
---
+++ readme.md
@@ -0,0 +1,6 @@
+version : 18.2.4 
+Major.Minor.patch
+
+^:  It includes Minor and patches
+~:  It includes patches only
+* No prefix: It includes only the exact version
