6eb1ec0269cba1fbc0977a3d5fe102b56987ffa3
sumitmishrathinksys <mishra.sumit@thinksys.com>
2025-04-23 14:54:32 +0530

    understand react basic

---
+++ app.js
@@ -0,0 +1,7 @@
+const element = React.createElement("h1", {}, "Hello Coder Army");
+
+// ReactDOM.render(element, document.getElementById("root"));
+
+const Reactroot = ReactDOM.createRoot(document.getElementById("root"));
+Reactroot.render(element);
+
